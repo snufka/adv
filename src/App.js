1a62ad638972ed1f3b57ac5369961525a92ec7e1
snufka <52801250+snufka@users.noreply.github.com>
2020-08-22 19:26:48 +0200

    header layout

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>hello world</h1>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
 import React from "react";
-import Background from "../img/background.JPG";
+import background from "../img/background.JPG";
+import Logo from "../img/logoadv.png";
 
 export default function Header() {
+  const headerImage = {
+    display: "fixed",
+  };
   return (
     <div>
-      <img src={Background} alt="background" style={{ width: "100%" }} />
+      <div className="row">
+        <div className="col">
+          <img src={Logo} alt="logo"></img>
+        </div>
+        <div className="col-6">
+          <h1 class="display-1">
+            Your next<br></br> digital level.
+          </h1>
+
+          <p>
+            <strong>Growth marketing scales your business model.</strong>
+          </p>
+          <button type="button" className="btn btn-dark">
+            Learn More
+          </button>
+        </div>
+        <div className="col-3">
+          <img src={background} alt="background" style={{}}></img>
+        </div>
+      </div>
     </div>
   );
 }
